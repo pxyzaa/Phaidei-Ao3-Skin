6ab9ccfcd02e739a2f4da72ff03cac51b3f81596
Fayza <fayza.alirhma@gmail.com>
2026-09-04 21:12:15 +0700

    Add files via upload

---
+++ PhaideiAo3Skin.cpp
@@ -0,0 +1,522 @@
+#header h1 sup,
+#greeting .icon,
+#header .button,
+#header .landmark,
+#header .logo {
+  display: none;
+}
+
+#header,
+#inner.wrapper {
+  margin: 0 4%;
+}
+
+#main,
+#inner.wrapper,
+.listbox .index,
+.work.own,
+.thread .even {
+  background: #fffaf3;
+}
+
+#outer.wrapper {
+  background: #b9d9f5;
+  padding-top: 140px;
+  background-image: url("https://raw.githubusercontent.com/pxyzaa/Phaidei-Ao3-Skin/refs/heads/main/Background1.jpg");
+  background-size: 30.7% auto;
+  background-repeat: repeat;
+  background-position: center top;
+}
+
+#header {
+  background: #dcecff;
+  border: 6px solid #4778ad;
+  border-bottom-color: #f0a875;
+  border-radius: 14px;
+  box-shadow: 0 3px 10px rgba(38, 55, 70, 0.18);
+}
+
+#header .heading {
+  margin: 18px 0 10px 18px;
+  border: 2px solid #4778ad;
+  background-color: #f5b98f;
+  border-radius: 0;
+  width: 130px;
+  height: 80px;
+  display: block;
+  overflow: hidden;
+}
+
+#header .heading a {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+#header .primary {
+  box-shadow: none;
+  background: transparent;
+  color: #263746;
+}
+
+#header .primary li:first-child {
+  margin-left: 0;
+}
+
+#header .primary li,
+#header .primary li a,
+#greeting li,
+#greeting li a {
+  color: #263746;
+}
+
+#header .heading a {
+  color: #3b5bbf;
+  font-size: 0;
+  padding: 0;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#header h1.heading a::after {
+  content: "";
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: url("https://raw.githubusercontent.com/pxyzaa/Phaidei-Ao3-Skin/refs/heads/main/PhaideiWhiteDay.png") no-repeat center;
+  background-size: 200% 200%;
+  background-position: center;
+}
+
+#greeting {
+  background: transparent;
+  margin-right: 0;
+  color: #263746;
+  padding: 18px;
+}
+
+#greeting li {
+  clear: left;
+  border-bottom: 1px dashed #f0a875;
+  margin-top: 0.3em;
+}
+
+#greeting .icon img {
+  margin-top: 0.3em;
+  border: 2px solid #fffaf3;
+}
+
+#greeting .dropdown .menu li {
+  margin: 0 0.125em;
+}
+
+#greeting .menu {
+  width: 16em;
+}
+
+#footer {
+  border: none;
+  background: transparent;
+}
+
+#footer .menu,
+#footer .menu a {
+  color: #315b87;
+}
+
+#footer h4,
+.blurb h4 a:link {
+  color: #4778ad;
+}
+
+#footer {
+  box-sizing: border-box;
+  width: auto;
+  margin: 20px 4% 0 4%;
+  padding: 25px 30px;
+  background: #f5b98f !important;
+  border: 3px solid #f0a875 !important;
+  border-radius: 14px;
+  box-shadow: 0 3px 8px rgba(38, 55, 70, 0.15);
+}
+
+#footer .menu,
+#footer .menu a {
+  color: #263746;
+}
+
+#footer .menu a:hover,
+#footer .menu a:focus {
+  background: #4778ad;
+  color: #fff;
+}
+
+#footer h4 {
+  color: #263746;
+}
+
+.system .latest h3,
+.splash .module h3,
+.system .tweets h3 {
+  color: #4778ad;
+}
+
+h2 {
+  font-size: 1.286em;
+  line-height: 1;
+  margin: 0.5375em 0;
+}
+
+#main a:hover,
+#main a:focus {
+  background: #f0a875;
+  color: #fff;
+  text-decoration: none;
+}
+
+li.blurb a:hover,
+li.blurb a:focus {
+  background: #f0a875;
+  color: #fff;
+  text-decoration: none;
+}
+
+li.blurb .header .heading a:hover,
+li.blurb .header .heading a:focus {
+  background: #f0a875;
+  color: #fff;
+}
+
+li.blurb .byline a:hover,
+li.blurb .byline a:focus {
+  background: #f0a875;
+  color: #fff;
+}
+
+li.blurb .tags a:hover,
+li.blurb .tags a:focus,
+a.tag:hover,
+a.tag:focus {
+  background: #f0a875;
+  color: #fff;
+}
+
+#dashboard a:hover,
+#dashboard a:focus,
+#dashboard .current {
+  background: #f0a875;
+  color: #fff;
+}
+
+#header .primary a:hover,
+#header .primary a:focus,
+#header .actions a:hover,
+#header .actions a:focus {
+  background: #f0a875;
+  color: #fff;
+}
+
+#header .actions a:hover,
+#header .actions a:focus,
+#header .dropdown:hover a,
+#header .open a,
+#header .dropdown .menu a,
+#header .dropdown .menu,
+#greeting .dropdown .menu,
+#greeting .dropdown .menu a {
+  background: #dcecff;
+  color: #263746;
+}
+
+#header .dropdown .menu a:hover,
+#header .dropdown .menu a:focus,
+#greeting .dropdown .menu a:hover,
+#greeting .dropdown .menu a:focus {
+  background: #f0a875;
+  color: #fff;
+}
+
+#header .dropdown .menu,
+#greeting .dropdown .menu {
+  border: 2px solid #f0a875;
+}
+
+#greeting .current,
+#greeting .dropdown:hover li,
+#greeting .dropdown:focus li {
+  box-shadow: none;
+  background: #ffe0c7;
+  color: #263746;
+}
+
+#dashboard,
+#dashboard.own {
+  background: #dcecff;
+  color: #263746;
+  border: 2px solid #4778ad;
+  border-right: 4px solid #f0a875;
+  box-shadow: 0 2px 6px rgba(38, 55, 70, 0.12);
+}
+
+#dashboard ul {
+  background: transparent;
+  border: none;
+}
+
+#dashboard .secondary {
+  background: #fffaf3;
+  box-shadow: none;
+}
+
+.splash .module h3,
+.splash .news li,
+.home .header h2,
+li.comment,
+dl.meta,
+.bookmark .user,
+#header .menu li,
+#dashboard ul,
+#dashboard .secondary,
+ul.notes,
+.flash,
+.notice,
+tbody tr,
+thead td,
+#header #small_login,
+fieldset,
+form dl,
+fieldset dl dl,
+fieldset fieldset fieldset,
+fieldset fieldset dl dl,
+dd.hideme,
+form blockquote.userstuff,
+form dt,
+.actions a:active,
+.current,
+a.current,
+.current a:visited,
+.search span.tip,
+.listbox,
+fieldset fieldset.listbox {
+  border-color: #8db5d9;
+}
+
+.comment h4.byline,
+#main .verbose legend,
+.unread,
+.child,
+.unwrangled,
+.unreviewed,
+.verbose fieldset,
+.draft,
+form ul.notes,
+#modal,
+.ui-sortable li,
+.comment_notice,
+.kudos_notice,
+div.dynamic,
+#ui-datepicker-div,
+.ui-datepicker table {
+  background: #d8e9f8;
+  border-color: #4778ad;
+}
+
+.flash,
+.notice {
+  background: #ffe0c7;
+  border-color: #f0a875;
+}
+
+form .notice,
+ul.notes,
+.caution {
+  background: #eaf4ff;
+  border-color: #4778ad;
+}
+
+.alert.flash,
+.error,
+.comment_error,
+.kudos_error {
+  background: #efd1d1;
+  color: #000;
+  border-color: #a44;
+}
+
+.listbox,
+.bookmark .user,
+.bookmark .user .header,
+.reading h4.viewed,
+li.relationships a,
+.dropdown,
+fieldset fieldset.listbox,
+fieldset,
+form dl,
+fieldset dl dl,
+fieldset fieldset fieldset,
+fieldset fieldset dl dl,
+dd.hideme,
+form blockquote.userstuff,
+.dynamic form fieldset,
+.toggled form fieldset,
+.toggled form dl,
+.secondary .toggled form,
+.secondary form,
+.required .autocomplete {
+  background: #eaf4ff;
+}
+
+.bookmark .user {
+  border-radius: 10px;
+}
+
+.filters .submit input {
+  color: #fff;
+  background: #4778ad;
+  border: 2px solid #f5b98f;
+  border-radius: 8px;
+  box-shadow: 0 2px 5px rgba(38, 55, 70, 0.2);
+}
+
+.filters .submit input:hover {
+  background: #f0a875;
+  color: #fff;
+  border-color: #4778ad;
+}
+
+.filters fieldset {
+  border-radius: 12px;
+}
+
+li.blurb,
+.listbox li.blurb,
+li.comment,
+.statistics .index li:nth-of-type(even),
+dl.meta,
+dl.index dd,
+#ibox_wrapper,
+#ibox_content,
+textarea:focus,
+input:focus,
+select:focus {
+  background: #dcecff;
+}
+
+textarea,
+input,
+select,
+option,
+.toggled form,
+.dynamic form,
+.autocomplete .dropdown ul,
+.autocomplete .dropdown ul li,
+.autocomplete input {
+  background: #fff;
+}
+
+li.blurb,
+.listbox li.blurb {
+  border: 3px solid #8db5d9;
+  border-left: 6px solid #f0a875;
+  border-radius: 12px;
+  box-shadow: 0 3px 8px rgba(38, 55, 70, 0.12);
+}
+
+li.blurb:hover {
+  border-left-color: #4778ad;
+}
+
+.blurb .header {
+  border-bottom: 2px solid #8db5d9;
+}
+
+.bookmark .user .module {
+  border: none;
+}
+
+.reading h4.viewed,
+.bookmark .user {
+  box-shadow: none;
+}
+
+.reading h4.viewed {
+  border: 1px dashed #f0a875;
+}
+
+.icon {
+  border: 2px solid #fffaf3;
+}
+
+.comment div.icon {
+  border-bottom: none;
+}
+
+.workskin .notes,
+dl.meta,
+.reading h4.viewed,
+.bookmark .user,
+.comment .posted,
+.comment .edited {
+  font-size: 90%;
+}
+
+#workskin {
+  text-align: justify;
+}
+
+#workskin h2 {
+  font-size: 2.7em;
+  color: #4778ad;
+  border-bottom: 3px solid #f0a875;
+  padding-bottom: 0.2em;
+}
+
+ul.tags li.warnings,
+ul.tags li.relationships,
+ul.tags li.characters,
+ul.tags li.freeforms {
+  float: left;
+  margin: 0;
+  padding: 2px;
+}
+
+li.warnings + li.relationships,
+li.relationships + li.characters,
+li.characters + li.freeforms,
+li.warnings + li.characters,
+li.warnings + li.freeforms,
+li.relationships + li.freeforms {
+  clear: left;
+}
+
+ul.tags li.warnings:first-child:before {
+  content: "☀️ ";
+  color: #f0a875;
+  font-family: Georgia, serif;
+  font-weight: 600;
+}
+
+li.warnings + li.relationships:before,
+li.relationships + li.characters:before,
+li.characters + li.freeforms:before,
+li.warnings + li.characters:before,
+li.warnings + li.freeforms:before,
+li.relationships + li.freeforms:before {
+  content: "🍷 ";
+  color: #f0a875;
+  font-family: Georgia, serif;
+  font-weight: 600;
+}
+
+ul.tags li.relationships a {
+  color: #4778ad;
+}
+
+ul.tags li a:hover,
+ul.tags li a:focus {
+  background: #f0a875;
+  color: #fff;
+}
